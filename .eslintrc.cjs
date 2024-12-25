ff37c52af240bfdaf12dc914ffcb672196fd24f6
TranVdc <tranvanduoc2394@gmail.com>
2024-12-25 14:21:56 +0700

    "@typescript-eslint/restrict-template-expressions": "off",

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ const config = {
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/no-unsafe-argument": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
