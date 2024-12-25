312ba412de3f651746f03833acdd5f577d304d31
DESKTOP-S3C5VP0\TRANVDC <tranvanduoc2394@gmail.com>
2024-12-25 22:16:18 +0700

    register user to db

--- next.config.js
+++ next.config.js
@@ -4,7 +4,22 @@
  */
 import "./src/env.js";
 
-/** @type {import("next").NextConfig} */
-const config = {};
-
-export default config;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          hostname: "lh3.googleusercontent.com",
+        },
+      ],
+    },
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+    reactStrictMode: false,
+  };
+  
+  export default nextConfig;
